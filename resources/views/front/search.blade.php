8045f55c84d7b56f3df9dd343e0e3161863acea7
xfshahrukh44 <ssidduit@gmail.com>
2023-11-04 05:54:56 +0500

    footer changes

--- resources/views/front/search.blade.php
+++ resources/views/front/search.blade.php
@@ -732,6 +732,7 @@
                                             <li id="menu-item-1545" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1545"><a href="{{env('MAIN_SITE_URL')}}/terms-of-use/">Terms of Use</a></li>
                                         </ul>
                                     </li>
+                                    <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-6433"><a href="{{env('MAIN_SITE_URL')}}/inquiry/">Customer Inquiry Form</a></li>
 
 
                                 </ul>
@@ -754,6 +755,7 @@
                                             <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1545"><a href="{{env('MAIN_SITE_URL')}}/terms-of-use/">Terms of Use</a></li>
                                         </ul>
                                     </li>
+                                    <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-6433"><a href="{{env('MAIN_SITE_URL')}}/inquiry/">Customer Inquiry Form</a></li>
                                 </ul>
                             </div>
 
@@ -761,11 +763,11 @@
 
 
 
-                            <div class="stm-c-f-add-btn-wrap">
-                                <a class="add-listing-btn stm-button heading-font"
-                                   href="https://gcejapan.jp/contact-us/">
-                                    <i class=" icon-ap-latter" style="color: ; font-size: 15px;"></i>                                    Contact Us                                </a>
-                            </div>
+{{--                            <div class="stm-c-f-add-btn-wrap">--}}
+{{--                                <a class="add-listing-btn stm-button heading-font"--}}
+{{--                                   href="https://gcejapan.jp/contact-us/">--}}
+{{--                                    <i class=" icon-ap-latter" style="color: ; font-size: 15px;"></i>                                    Contact Us                                </a>--}}
+{{--                            </div>--}}
                         </div>
                     </div>
                 </div>
